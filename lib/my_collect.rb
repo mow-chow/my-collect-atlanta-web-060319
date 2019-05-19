6758f37d9bc943ddee51db3f45baae61f8c727c7
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-19 02:36:20 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@
 collection = ['ruby', 'javascript', 'python', 'objective-c']
 array = []
 def my_collect(collection) do |lang|  lang.upcase
-  while i == 0
+  while i < array.length
     block_given?
     array << yield(collection[i])
     i += 1
